0b1cf87a30b03f1f61c414f73692c873eef40dfa
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-03 21:07:15 +0100

    feat: add es iniziali

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+const newArray = []
+
+function arrayTwo()  {
+    for (let i = 0; i <= names.length -1; i++) {
+        const letterNumberOne = names[i].substring(0,1);
+        newArray.push(letterNumberOne);
+
+    }
+        return newArray;
+
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(arrayTwo(newArray));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
