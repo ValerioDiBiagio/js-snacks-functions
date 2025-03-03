94bc85a645e485b4ab6e75e4c5a7c702c888e4ee
ValerioDiBiagio <dibiagiovalerio@gmail.com>
2025-03-03 22:38:48 +0100

    feat: add es conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,26 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function numOfvolws(string) {
+
+    const volwes = ['a','e','i','o','u'];
+
+    let volwsNumber = 0;
+
+    for (let i = 0; i < string.length; i++) {
+
+        for (let v = 0; v < volwes.length; v++) {
+            if (string[i]=== volwes[v]) volwsNumber++;
+        }
+    }
+
+    return volwsNumber;
+}
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(numOfvolws(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
